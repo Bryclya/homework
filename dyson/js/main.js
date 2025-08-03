fe344e1f48270010812b1c63eba595eaec64ccaf
Bryclya <kirill.de44in@yandex.ru>
2025-08-03 22:43:28 +0300

    Add files via upload

--- dyson/js/main.js
+++ dyson/js/main.js
@@ -1,23 +1,44 @@
-
 //калькулятор корзины
 
 
-const subtract = document.querySelector ('.offers__numders-subtract')
-const add = document.querySelector ('.offers__numders-add')
-const indicator = document.querySelector ('.offers__numders-indicator')
-let number = parseFloat(indicator.textContent)
+const subtract = document.querySelector('.offers__numders-subtract-icon');
+const subtractIcon = document.querySelector('.offers__numders-subtract');
+const add = document.querySelector('.offers__numders-add-icon');
+const indicator = document.querySelector('.offers__numders-indicator');
 
-if (number === 1) {
-    subtract.style.color = 'red'
-    console.log (subtract)
-}
-add.addEventListener ('click', () => {
-    let mathAdd = toString(number++)
-    indicator.textContent = number
-})
-subtract.addEventListener ('click', () => {
-    if (number>1) {
-        let mathSub = toString(number--)
-        indicator.textContent = number
+let number = parseFloat(indicator.textContent);
+
+add.addEventListener('click', () => {
+    number++;
+    indicator.textContent = number;
+    if (number > 1) {
+        subtractIcon.classList.remove('offers__numders-subtract--inactive');
     }
-})
+});
+
+subtract.addEventListener('click', () => {
+    if (number > 1) {
+        number--;
+        indicator.textContent = number;
+    }
+    if (number === 1) {
+        subtractIcon.classList.add('offers__numders-subtract--inactive');
+    }
+});
+
+// Слайдер
+
+
+const swiper = new Swiper('.offers__slider', {
+
+    slidesPerView: 1,
+    
+    pagination: {
+        el: '.offers-pagination',
+        type: 'fraction'
+    },
+    navigation: {
+        nextEl: '.offers__swiper-button-next',
+        prevEl: '.offers__swiper-button-prev',
+    },
+}); 
