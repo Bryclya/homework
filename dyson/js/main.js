fdf011a9389d5f8dd6d5fcbfaa3f27242df83c3f
Bryclya <kirill.de44in@yandex.ru>
2025-08-05 00:51:52 +0300

    Add files via upload

--- dyson/js/main.js
+++ dyson/js/main.js
@@ -42,3 +42,25 @@ const swiper = new Swiper('.offers__slider', {
         prevEl: '.offers__swiper-button-prev',
     },
 }); 
+
+
+// Аккордеон
+
+const accordionLists = document.querySelectorAll('.accordion-list')
+
+accordionLists.forEach (el => {
+    el.addEventListener('click', (e) => {
+        const accordionControl = e.target.closest('.accordion-list__control')
+        if (!accordionControl) return
+        const accordionItem = accordionControl.parentElement
+        const accordionContent = accordionControl.nextElementSibling
+
+        accordionItem.classList.toggle('accordion-list__item--opened')
+
+        if (accordionItem.classList.contains('accordion-list__item--opened')) {
+            accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'
+        } else {
+            accordionContent.style.maxHeight = null
+        }
+    })
+})
