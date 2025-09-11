f504edfe3061735577ac8ac95ce4383df71f6e98
Bryclya <kirill.de44in@yandex.ru>
2025-09-11 19:24:47 +0300

    Add files via upload

--- lesson JS 1/main.js
+++ lesson JS 1/main.js
@@ -1,12 +1,20 @@
-//задача 1
+// Задача 1
 const firstName = 'kirill';
 const lastName = 'Demin';
 const isStudent = true;
 
-//задача 2
+// Задача 2
 let age = 22;
 let currentYear = 2025;
 const birthYear = currentYear - age;
 
-//задача 3
+// Задача 3
 console.log(`Меня зовут ${firstName } ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}`)
+
+// Задача 4
+// let a = '123' - string
+// let b = +'456' - number
+// let c = Number('789') - number
+// let d = Boolean(0) - boolean
+// let e = Boolean(' ') - boolean
+// let result = a + b + c + D + e - string
