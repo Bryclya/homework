f06bf23624f2784825aaf69b7df6f663950854a4
Bryclya <kirill.de44in@yandex.ru>
2025-09-12 21:56:55 +0300

    Add files via upload

---
+++ lesson JS 3/main.js
@@ -0,0 +1,18 @@
+// Задача 1
+for (let i=1; i<=20; i++) {
+    if (i%4==0) {
+        continue 
+    }
+    console.log(i)
+}
+
+// Задача 2
+const number = +prompt("Введите число:");
+let factorial = 1
+
+for (let i = 1; i <= number; i++) {
+  factorial *= i;
+}
+
+// alert(`Факториал числа равен ${factorial}`);
+
